98b06bd1229fbc04f216d81693f250eb559dfe9c
Ryan Lertola <rlertola@gmail.com>
2018-12-17 23:25:53 -0600

    Next commit

--- js/app.js
+++ js/app.js
@@ -43,3 +43,67 @@ const displayColors = (id, string) => {
   })
 }
 
+let amountArray = [];
+// Register for Activities
+$('.activities').on('change', (e) => {
+  const parent = e.target.parentElement;
+  const string = parent.textContent;
+
+  const amountRegex = /\d{3}/;
+  const amount = string.match(amountRegex)[0];
+  const amountInt = parseInt(amount);
+
+  let day;
+  const dayRegex = /(mon|tues|wed|thur|fri)(\w+)/i;
+  if (dayRegex.test(string)) {
+    day = string.match(dayRegex)[0];
+  };
+
+  let time;
+  const timeRegex = /\d\d?(am|pm)-?\d\d?(am|pm)?/;
+  if (timeRegex.test(string)) {
+    time = string.match(timeRegex)[0];
+  }
+
+  const $labels = $('.activities').children();
+
+  if (e.target.checked) {
+    amountArray.push(amountInt);
+  } else {
+    amountArray.pop(amountInt);
+  }
+
+  $labels.each((i, label) => {
+    let string = label.textContent;
+    let timeLabel = string.match(timeRegex);
+    let dayLabel = string.match(dayRegex);
+    if (timeLabel !== null) {
+      timeLabel = string.match(timeRegex)[0];
+      dayLabel = string.match(dayRegex)[0];
+      if (timeLabel === time && dayLabel === day) {
+        if (e.target.checked === true) {
+          label.className = 'timeConflict';
+          parent.className = 'inherit';
+        } else {
+          label.className = 'inherit';
+        }
+      }
+    }
+  })
+  $('#total').text(`Total: $${getTotal(amountArray)}`);
+})
+
+// Gets total of things checked.
+const getTotal = (amtsArray) => {
+  if (amtsArray.length > 0) {
+    return amtsArray.reduce((a, b) => {
+      return a + b;
+    })
+  } else {
+    return 0;
+  }
+}
+
+
+
+
